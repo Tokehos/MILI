e9d90fd114732e67b4f0c0f5b6169df99f4271f2
Tokehos <88163971+Tokehos@users.noreply.github.com>
2021-09-16 09:52:27 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 2000; // стоимость билета //
+        int milli = 20;  // цена за 1 милю //
+        int milliBalance = cost / milli;
+        System.out.println(milliBalance);
+    }
+
+}
+
